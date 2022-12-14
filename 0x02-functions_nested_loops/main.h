e84d06b002c5f14eedb5b738b1b5094aa5d1395f
joel magana <joelmagana15@gmail.com>
2022-12-14 00:33:01 -0800

    task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char);
+void print_alphabet(void);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int print_sign(int n;)
+int _isalpha(int c);
+int _islower(int c);
+void print_alphabet_x10(void);
+int _abs(int);
